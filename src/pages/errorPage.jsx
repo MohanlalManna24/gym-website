e3eedb78c9a2086834022f73158d25c8d5a4eb60
MohanlalManan24 <mohanlalmanna.bca2024@gmail.com>
2026-03-31 15:42:39 +0530

    create errorHandeling page

---
+++ src/pages/errorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const errorPage = () => {
+  return (
+    <div>
+      <h1>Error - Something went wrong</h1>
+      <p>An error occurred while loading the page.</p>
+    </div>
+  )
+}
+
+export default errorPage
