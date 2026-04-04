1c1e9f5c1f3fd780ed27ecff452beb443b183383
MohanlalManan24 <mohanlalmanna.bca2024@gmail.com>
2026-04-04 11:22:10 +0530

    Create ScrollToTop component that is use to automatically scrolls to the top whenever a route change

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../component/Header";
 import Navbar from "../component/Navbar";
 import Footer from "../component/Footer";
+import ScrollToTop from "../utils/ScrollToTop";
 
 const RootLayout = () => {
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Navbar />
       <Outlet />
