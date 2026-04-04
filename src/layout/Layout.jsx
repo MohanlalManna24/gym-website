95ab33376e0386786cd6531ca1c152fa3372b88a
MohanlalManan24 <mohanlalmanna.bca2024@gmail.com>
2026-04-04 14:25:01 +0530

    layout component: fixed mobile responsive design add padding

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({ children }) => {
   return (
-    <div className='max-w-7xl mx-auto '>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8 lg:px-10'>
       {children}
     </div>
   )
